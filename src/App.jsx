62dfd9a3a2d8da268ed60a1d9b6f101d139b326f
Shahriar Ahmed <chatpgt7@gmail.com>
2025-10-24 01:48:39 +0600

    initial setup. added tailwind and all the pages and components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Experience from "./sections/Experience";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+
+
+export default function App() {
+  return <div className="relative gradient-bg text-white">
+    <Navbar />
+    <Home />
+    <About />
+    <Experience />
+    <Skills />
+    <Projects />
+    <Testimonials />
+    <Contact />
+    <Footer />
+  </div>;
+}
