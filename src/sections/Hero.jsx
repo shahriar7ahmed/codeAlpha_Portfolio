c57ab0e3a16b83296b008b0ed4a51b985c35c375
Shahriar Ahmed <chatpgt7@gmail.com>
2025-12-02 17:27:21 +0600

    removed scroll indicator button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -147,18 +147,6 @@ const Hero = () => {
           </motion.div>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, repeat: Infinity, repeatType: 'reverse' }}
-        className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
-      >
-        <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-gray-400 rounded-full mt-2 animate-bounce"></div>
-        </div>
-      </motion.div>
     </section>
   )
 }
